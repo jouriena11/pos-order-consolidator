66b6451bc700a2a4f880cf99219b5b13792c021c
jouriena11 <117335220+jouriena11@users.noreply.github.com>
2023-08-22 18:44:08 +1000

    edited connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pos-order-consolidator');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://pos-mongo:27017/pos-order-consolidator');
 
 module.exports = mongoose.connection;
 
