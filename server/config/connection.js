9e7e9fda63ed688a0890561922771100c96f0a92
jouriena11 <117335220+jouriena11@users.noreply.github.com>
2023-08-23 14:02:05 +1000

    updated docker-compose and connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://172.31.8.97:27017/pos-order-consolidator');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pos-order-consolidator');
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pos-order-consolidator');
 
 module.exports = mongoose.connection;
