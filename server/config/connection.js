5dac2908d0de39aa43a25fcf419a9e1e4d465d2b
jouriena11 <117335220+jouriena11@users.noreply.github.com>
2023-08-23 12:58:25 +1000

    updated connection.js and docker-compose.yml

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,5 +5,3 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://172.31.8.97:27017/pos-ord
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pos-order-consolidator');
 
 module.exports = mongoose.connection;
-
-t 
